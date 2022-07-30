f6ef3a418e5aa8e45b46c77ed208436753720e91
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2022-07-30 00:13:30 -0700

    removed logger helper

--- src/helpers/utils/connectToDb.ts
+++ src/helpers/utils/connectToDb.ts
@@ -1,12 +1,11 @@
 import mongoose from 'mongoose';
-import log from './logger';
 
 async function connectToDb() {
     const dbUri = <string>process.env.DB_URI;
 
     try {
         await mongoose.connect( dbUri );
-        log.info( 'Connected to DB' );
+        console.log( 'Connected to DB' );
     } catch ( e ) {
         process.exit( 1 );
     }
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import app from './app';
-import log from './helpers/utils/logger';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require( 'dotenv' ).config();
 
 const port = process.env.PORT || 3000;
 
 app.listen( port, () => {
-    log.info( `App started on port ${port}` );
+    console.log( `App started on port ${port}` );
 } );
