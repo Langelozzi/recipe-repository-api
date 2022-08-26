e8c00d22affd26c3cdedeef7c097060059937c42
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2022-08-25 18:52:19 -0700

    configured vercel deployment

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,13 @@ import cors from 'cors';
 import { AuthRoutes } from './routes/auth.routes';
 import connectToDb from './helpers/utils/connectToDb';
 import { RecipeRoutes } from './routes/recipe.routes';
+import { IndexRoutes } from './routes/index.routes';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require( 'dotenv' ).config();
 class App {
     public app: express.Application;
+    public indexRoutes: IndexRoutes = new IndexRoutes();
     public authRoutes: AuthRoutes = new AuthRoutes();
     public recipeRoutes: RecipeRoutes = new RecipeRoutes();
     
@@ -17,6 +19,7 @@ class App {
         this.app = express();
         this.config();
         connectToDb();
+        this.indexRoutes.routes( this.app );
         this.authRoutes.routes( this.app );
         this.recipeRoutes.routes( this.app );
     }
