be37c2d31f8077f0da4f61fce998d17c59e670c1
Langelozzi <lucasangelozzi32@gmail.com>
2025-12-16 13:41:25 -0800

    Attempt 1: fixing image upload error via cors updates

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,11 @@ import app from './app';
 import { VercelRequest, VercelResponse } from '@vercel/node';
 require('dotenv').config();
 
-// const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-// app.listen(port, () => {
-//     console.log(`App started on port ${port}`);
-// });
+app.listen(port, () => {
+    console.log(`App started on port ${port}`);
+});
 
 export default (req: VercelRequest, res: VercelResponse) => {
     app(req as any, res as any);
